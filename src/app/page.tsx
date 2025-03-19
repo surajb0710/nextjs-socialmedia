f075b2d87abe63a558913fcb42472fb61dd476fc
Suraj <104007414+surajb0710@users.noreply.github.com>
2025-03-19 13:31:28 +0530

    Delete Post, notifications added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { getPosts } from '@/actions/post.action';
 import { getDbUserId } from '@/actions/user.action';
 import CreatePost from '@/components/CreatePost';
 import ModeToggle from '@/components/ModeToggle';
+import PostCard from '@/components/PostCard';
 import { Button } from '@/components/ui/button';
 import WhoToFollow from '@/components/WhoToFollow';
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
@@ -8,7 +10,7 @@ import { currentUser } from '@clerk/nextjs/server';
 
 const Home = async () => {
   const user = await currentUser();
-  // const posts = await getPosts();
+  const posts = await getPosts();
   const dbUserId = await getDbUserId();
 
   return (
@@ -16,11 +18,11 @@ const Home = async () => {
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
 
-        {/* <div className="space-y-6">
+        <div className="space-y-6">
           {posts.map((post) => (
             <PostCard key={post.id} post={post} dbUserId={dbUserId} />
           ))}
-        </div> */}
+        </div>
       </div>
 
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
